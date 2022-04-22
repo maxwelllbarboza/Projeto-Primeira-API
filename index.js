9672e542697f2745a1b572d8df2086c20273aeeb
maxwellbarboza@yahoo.com <maxwellbarboza@yahoo.com>
2022-04-22 19:54:13 -0300

    Criei a rota POST e PATCH

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 // GET /instrutores
 // GET /instrutores/5
@@ -32,6 +33,7 @@ const listaDeInstrutores = [
   },
 ];
 
+let proximoId = 5;
 
 app.get("/instrutores", (req, res)=>{
   res.json(listaDeInstrutores);
@@ -40,6 +42,35 @@ app.get("/instrutores", (req, res)=>{
 app.get("/instrutores/:idConsultado", (req, res)=>{
   const instrutor = listaDeInstrutores.find(instrutor => 
     instrutor.id === Number(req.params.idConsultado));
+  res.json(instrutor);
+});
+
+// POST /instrutores
+app.post("/instrutores", (req, res)=> {
+  const novoInstrutor = {
+    id: proximoId,
+    nome: req.body.nome,
+    idade: req.body.idade,
+    materia: req.body.materia,
+  }
+  listaDeInstrutores.push(novoInstrutor);
+  proximoId += 1;
+  res.json(novoInstrutor);
+});
+
+
+app.patch("/instrutores/:idConsultado", (req, res) => {
+  const instrutor = listaDeInstrutores.find(instrutor => 
+    instrutor.id === Number(req.params.idConsultado));
+  if(req.body.nome !== undefined){
+    instrutor.nome = req.body.nome;
+  }  
+  if(req.body.idade !== undefined){
+    instrutor.idade = req.body.idade;
+  }  
+  if(req.body.materia !== undefined){
+    instrutor.materia = req.body.materia;
+  }  
   res.json(instrutor);
 });
 
